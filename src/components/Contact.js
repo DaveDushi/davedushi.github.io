173f0bc56a36166c8ac7955fa6684d2cd0d9efda
DaveDushi <david.dusi@hotmail.com>
2024-05-27 13:09:29 +0300

    email service working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    user_name: "",
+    user_email: "",
     message: "",
   });
-  const [responseMessage, setResponseMessage] = useState("");
+
+  const formRef = useRef(); // Create a reference to the form element
+
+  useEffect(() => {
+    emailjs.init("UGi6TWQyWE0oo-NPA");
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,21 +25,19 @@ function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios
-      .post("/send-email", formData)
-      .then((response) => {
-        setResponseMessage(response.message);
+    emailjs
+      .sendForm("default_service", "contact_form", formRef.current) // Pass the form element reference
+      .then(() => {
         setFormData({
-          name: "",
-          email: "",
+          user_name: "",
+          user_email: "",
           message: "",
         });
-        alert(responseMessage);
+        alert("Message Sent!");
       })
-      .catch((error) => {
-        console.error("Error sending message:", error);
-        setResponseMessage("Couldn't send your message. Please try again.");
-        alert(responseMessage);
+      .catch((e) => {
+        console.log(e);
+        alert("Couldn't send message. Please try again later.");
       });
   };
 
@@ -46,30 +49,33 @@ function Contact() {
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Contact Me</h2>
-          <form onSubmit={handleSubmit}>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            {" "}
+            {/* Attach the reference to the form */}
             <div className="form-control">
-              <label className="label" htmlFor="name">
+              <input type="hidden" name="contact_number" value="1" />
+              <label className="label" htmlFor="user_name">
                 <span className="label-text">Name</span>
               </label>
               <input
                 type="text"
-                id="name"
-                name="name"
-                value={formData.name}
+                id="user_name"
+                name="user_name"
+                value={formData.user_name}
                 onChange={handleChange}
                 className="input input-bordered"
                 required
               />
             </div>
             <div className="form-control">
-              <label className="label" htmlFor="email">
+              <label className="label" htmlFor="user_email">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
-                id="email"
-                name="email"
-                value={formData.email}
+                id="user_email"
+                name="user_email"
+                value={formData.user_email}
                 onChange={handleChange}
                 className="input input-bordered"
                 required
